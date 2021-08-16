26c70f67196f8bae8ef5a368a706047e252d9212
Swagata Chakraborty <79792530+swagatac652@users.noreply.github.com>
2021-08-16 14:51:38 +0530

    new files

---
+++ portfolioJS.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+});
